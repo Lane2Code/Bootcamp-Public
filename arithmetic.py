2a3c2beed433b59103ce7614d09ac320c9189848
Lane2Code <92408522+Lane2Code@users.noreply.github.com>
2021-12-19 14:27:01 -0500

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+# imported by calculations.py in same folder
+
+def add(x, y):
+    return x + y
+def multiply(x, y):
+    return x * y
+def subtract(x, y):
+    return x - y
+
---
+++ calculations.py
@@ -0,0 +1,6 @@
+# Imports arithmetic.py in same folder
+
+import arithmetic
+print("20 plus 10 is ", arithmetic.add(20, 10))
+print("20 minus 10 is ", arithmetic.subtract(20, 10))
+print("20 times 10 is ", arithmetic.multiply(20, 10))
